0297475f196fa048fd0b4c0eaa15b95e0cdb0743
Ignashov Vadim Maksimovich <48078801+ImSOLty@users.noreply.github.com>
2024-10-14 17:40:30 +0300

    Add actions module to setup.py

--- Task Tracker/setup.py
+++ Task Tracker/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='task-cli',
     version='0.0.1',
-    py_modules=["task_manager", "args_parser", "classes", "file_manager", "task_manager"],
+    py_modules=["task_manager", "args_parser", "classes", "file_manager", "task_manager", "actions"],
     entry_points={
         'console_scripts': [
             'task-cli=task_manager:main'
