8d0e030dcbc0d0cd30a329e60b66bc389de9450e
Ignashov Vadim Maksimovich <48078801+ImSOLty@users.noreply.github.com>
2024-10-14 14:21:18 +0300

    Task Tracker: Add typing and method descriptions

--- Task Tracker/args_parser.py
+++ Task Tracker/args_parser.py
@@ -1,13 +1,16 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 import typing
 import json
 
 
 class Argument:
+    """
+    "Argument" class represents argument passed to the parser/subparser
+    """
     sub_parsers: list["Parser"]
     options: dict[str, typing.Any]
 
-    def __init__(self, options):
+    def __init__(self, options) -> "Argument":
         self.sub_parsers = {}
         if 'subs' in options:
             self.sub_parsers = [Parser.parse_config(parser) for parser in options['subs']]
@@ -15,7 +18,11 @@ class Argument:
         else:
             self.options = options
 
-    def construct_argument(self, parser: ArgumentParser):
+    def construct_argument(self, parser: ArgumentParser) -> None:
+        """
+        Add an argument to passed parser if there is no subparsers, depending on this argument.
+        Otherwise, constructs all related subparsers
+        """
         if self.sub_parsers:
             subparsers = parser.add_subparsers(**self.options)
             for sub_parser in self.sub_parsers:
@@ -25,21 +32,30 @@ class Argument:
 
 
 class Parser:
+    """
+    "Parser" class represents parser/subparser used to validate passed data
+    """
     options: dict[str, typing.Any]
     args: list[Argument]
 
-    def __init__(self, options):
+    def __init__(self, options: dict[str, typing.Any]) -> "Parser":
         self.options = options
         self.args = []
 
     @classmethod
-    def parse_config(cls, config):
+    def parse_config(cls, config: dict[str, typing.Any]) -> "Parser":
+        """
+        Class method used to create the instance of "Parser" class from the config passed through arguments
+        """
         res = cls({option: value for option, value in config.items() if option != 'args'})
         for options in config['args']:
             res.args.append(Argument(options))
         return res
 
-    def construct_parser(self, prev_parser=None):
+    def construct_parser(self, prev_parser: typing.Optional["Parser"] = None) -> ArgumentParser:
+        """
+        Method, used to construct the result ArgumentParser
+        """
         if prev_parser is None:
             parser = ArgumentParser(**self.options)
             for arg in self.args:
@@ -51,8 +67,14 @@ class Parser:
                 arg.construct_argument(subparser)
 
 
-def parse_arguments(config_path):
+def parse_arguments(config_path: str) -> Namespace:
+    """
+    Constructs an ArgumentParser based on the provided config file (path)
+    and returns all the parsed_args via argparse's Namespace object
+    """
+
     with open(config_path, 'r') as config_file:
         config = json.loads(config_file.read())
-        parser = Parser.parse_config(config).construct_parser()
-    print(parser.parse_args())
+    
+    parser = Parser.parse_config(config).construct_parser()
+    return parser.parse_args()
