a42fbb0de1b83383ec9f541b29259343a512fae6
Ignashov Vadim Maksimovich <48078801+ImSOLty@users.noreply.github.com>
2024-10-14 17:38:38 +0300

    update gitignore and setup.py

--- Task Tracker/setup.py
+++ Task Tracker/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='task-cli',
     version='0.0.1',
-    py_modules=["task_manager"],
+    py_modules=["task_manager", "args_parser", "classes", "file_manager", "task_manager"],
     entry_points={
         'console_scripts': [
             'task-cli=task_manager:main'
