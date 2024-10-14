96b68e0c5b78b125240f11063b226bed408520a6
Ignashov Vadim Maksimovich <48078801+ImSOLty@users.noreply.github.com>
2024-10-14 17:43:23 +0300

    Update modules

--- Task Tracker/setup.py
+++ Task Tracker/setup.py
@@ -2,12 +2,18 @@ from setuptools import setup
 
 setup(
     name='task-cli',
-    version='0.0.1',
-    py_modules=["task_manager", "args_parser", "classes", "file_manager", "task_manager", "actions"],
+    version='0.0.2',
+    py_modules=[
+        "task_manager",
+        "args_parser",
+        "classes",
+        "file_manager",
+        "task_manager",
+        "actions",
+    ],
     entry_points={
         'console_scripts': [
             'task-cli=task_manager:main'
         ]
     }
-
 )
